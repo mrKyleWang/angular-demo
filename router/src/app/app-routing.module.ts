2a609f295d397d743e6ea07c25ceee38a29c4eea
KyleWang <wk52525@126.com>
2018-05-10 12:40:30 +0800

    重定向路由

--- router/src/app/app-routing.module.ts
+++ router/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ProductComponent} from "./product/product.component";
 import {Code404Component} from "./code404/code404.component";
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'product/:id',component:ProductComponent},
-  {path:'**',component:Code404Component}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'product/:id', component: ProductComponent},
+  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
