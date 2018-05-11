d72546e0a2b4806b5733a2f9de3b5ae001d5d2ae
KyleWang <wk52525@126.com>
2018-05-11 12:24:52 +0800

    路由守卫

---
+++ router/src/app/guard/login.guard.ts
@@ -0,0 +1,14 @@
+import {CanActivate} from "@angular/router";
+
+export class LoginGuard implements CanActivate {
+
+  canActivate() {
+
+    let loggedIn : boolean = Math.random() < 0.5;
+
+    if (!loggedIn){
+      alert("用户未登录");
+    }
+    return loggedIn;
+  }
+}
