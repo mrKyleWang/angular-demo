b7770c4ce4f81ed80424518299dde2a00c148ab8
KyleWang <wk52525@126.com>
2018-05-14 18:56:48 +0800

    商品详情组件

--- auction/src/app/product-detail/product-detail.component.ts
+++ auction/src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  productTitle: String;
+
+  constructor(private routeInfo:ActivatedRoute) { }
 
   ngOnInit() {
+    this.productTitle = this.routeInfo.snapshot.params["productTitle"];
   }
 
 }
