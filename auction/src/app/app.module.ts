1b69a804f081fd3ec0b0061e6f6b6b709105dfbe
KyleWang <wk52525@126.com>
2018-05-14 19:04:06 +0800

    商品点击路由

--- auction/src/app/app.module.ts
+++ auction/src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { SearchComponent } from './search/search.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ProductComponent } from './product/product.component';
-import { StarsComponent } from './stars/stars.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {SearchComponent} from './search/search.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {ProductComponent} from './product/product.component';
+import {StarsComponent} from './stars/stars.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routeConfig: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product/:productTitle', component: ProductDetailComponent}
+]
 
 @NgModule({
   declarations: [
@@ -24,9 +30,11 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
