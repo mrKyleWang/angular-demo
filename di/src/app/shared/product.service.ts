12e514f410dcd03098f29f0ecba6d1e55af9f88d
KyleWang <wk52525@126.com>
2018-05-15 13:28:10 +0800

    初始化

---
+++ di/src/app/shared/product.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() {
+  }
+
+  getProduct(): Product {
+    return new Product(0, "iPhoneX", 6899, "最新款苹果手机");
+  }
+}
+
+export class Product {
+
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public desc: string
+  ) {
+
+  }
+
+}
