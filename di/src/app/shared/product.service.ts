a7bcb637c3092017214f5a1053b20bb92ea05ce0
KyleWang <wk52525@126.com>
2018-05-16 13:38:23 +0800

    服务注入

--- di/src/app/shared/product.service.ts
+++ di/src/app/shared/product.service.ts
@@ -1,12 +1,14 @@
 import {Injectable} from '@angular/core';
+import {LoggerService} from "./logger.service";
 
 @Injectable()
 export class ProductService {
 
-  constructor() {
+  constructor(private logger: LoggerService) {
   }
 
   getProduct(): Product {
+    this.logger.log("getProduct()方法被调用");
     return new Product(0, "iPhoneX", 6899, "最新款苹果手机");
   }
 }
