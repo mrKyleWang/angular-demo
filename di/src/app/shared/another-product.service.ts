824b53740e037bb1f0e5436bed61fa45b5198e80
KyleWang <wk52525@126.com>
2018-05-16 13:34:30 +0800

    覆盖注入

--- di/src/app/shared/another-product.service.ts
+++ di/src/app/shared/another-product.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {Product, ProductService} from "./product.service";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AnotherProductService {
+@Injectable()
+export class AnotherProductService implements ProductService{
 
   constructor() { }
+
+  getProduct(): Product {
+    return new Product(1, "华为P20", 3899, "最新款华为手机");
+  }
 }
